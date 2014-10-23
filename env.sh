fd4ae2d6e4770c84a3335f40e9504fc651c978d2
Samer Atiani <satiani@gmail.com>
2014-10-23 12:38:07 +0300

    Add instructions to run the yellowdemo instance

---
+++ env.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+export DEBUG=1
+export DJANGO_PROJECT=yellowdemo
+# This is a dummy SECRET_KEY provided for convenience, make sure you change if you
+# ever run this on a public facing website
+export SECRET_KEY=375e64bf082645951ba9fc1ced1e3f4f
