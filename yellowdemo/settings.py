f0499c8fec785ab926fe3fcdb43f7c6eaab84d37
James Piechota <piechota@gmail.com>
2015-03-19 15:34:10 -0700

    set ALLOWED_HOSTS so we can disable DEBUG
    references #209

--- yellowdemo/settings.py
+++ yellowdemo/settings.py
@@ -16,7 +16,8 @@ MANAGERS = ADMINS
 
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [ 'yellow-demo.herokuapp.com',
+                  'yellow-demo-stage.herokuapp.com' ]
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
