0602a46b0de6301a519170dd6afeb17c2d842d5f
James Piechota <piechota@gmail.com>
2015-03-23 10:44:05 -0700

    add currencies to demo

--- demo/forms.py
+++ demo/forms.py
@@ -1,6 +1,17 @@
 from django import forms
 
 class CreateInvoiceForm(forms.Form):
-    currency = forms.ChoiceField(choices=[('USD', 'USD'), ('AED', 'AED')])
+    currency = forms.ChoiceField(choices=[('USD', 'USD'),
+                                          ('AED', 'AED'),
+                                          ('BHD', 'BHD'),
+                                          ('DZD', 'DZD'),
+                                          ('EGP', 'EGP'),
+                                          ('JOD', 'JOD'),
+                                          ('LBP', 'LBP'),
+                                          ('MAD', 'MAD'),
+                                          ('OMR', 'OMR'),
+                                          ('QAR', 'QAR'),
+                                          ('SAR', 'SAR'),
+                                          ('TND', 'TND'),
+                                          ])
     amount = forms.DecimalField()
-    redirect = forms.BooleanField(required=False)
